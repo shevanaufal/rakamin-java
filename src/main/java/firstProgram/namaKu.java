010c603237c76345bedba8a49cc339f02bc52d0b
Sheva Naufal <shevanaufalrifqi28@gmail.com>
2023-10-18 20:11:18 +0700

    Changes namaKu java, penambahan wording

--- src/main/java/firstProgram/namaKu.java
+++ src/main/java/firstProgram/namaKu.java
@@ -3,5 +3,6 @@ package firstProgram;
 public class namaKu {
     public static void main(String[] args){
         System.out.println("Namaku Sheva");
+        System.out.println("Ada perubahan nih gaes");
     }
 }
